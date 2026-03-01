74b6229a71b3b2c773b950640d05a6fd6537dbc2
Aditya Anand <contactaditechdev@gmail.com>
2026-03-01 19:50:09 +0530

    Done same setup using composition

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 /** @format */
 
+// import { CsvFileReader } from "./CsvFileReader";
+// import { MatchReader } from "./MatchReader";
+// import { CsvFileReader } from "./CsvFileReader";
 import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./matchResult";
 
-const reader = new MatchReader("football.csv");
-reader.read();
+// const reader = new MatchReader("football.csv");
+// reader.read();
+
+// Create an object that satifies  the DataReader interface
+
+const csvFileReader = new CsvFileReader("football.csv");
+
+const matchReaded = new MatchReader(csvFileReader);
+matchReaded.load();
+matchReaded.matches;
 
 // const matches = reader.data;
 
@@ -16,7 +27,7 @@ console.log(MatchResult.AwayWin);
 let maWin = 0;
 const compareData: string = "Man United";
 
-for (let match of reader.data) {
+for (let match of matchReaded.matches) {
 	if (match[1] === compareData && match[5] === MatchResult.HomeWin) {
 		maWin++;
 	} else if (match[2] === compareData && match[5] === MatchResult.AwayWin) {
